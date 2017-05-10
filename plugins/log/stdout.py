67c0b100a511634398cd9fe1de7e8c8033d63651
Desobediente Civil <desci@riseup.net>
2017-05-10 00:10:36 -0300

    Moving log facility

---
+++ plugins/log/stdout.py
@@ -0,0 +1,27 @@
+# vim:fileencoding=utf-8
+## Standard output debugging
+
+import datetime
+
+class stdout():
+	def __init__(self):
+		pass
+	def debug(self, message):
+		now = str(datetime.datetime.now())
+		print '[%s] DEBUG: %s' % (now, message)
+	def info(self, message):
+		now = str(datetime.datetime.now())
+		print '[%s] INFO: %s' % (now, message)
+	def err(self, message):
+		now = str(datetime.datetime.now())
+		print '[%s] ERR: %s' % (now, message)
+	def cmd(self, command):
+		now = str(datetime.datetime.now())
+		print '[%s] CMD: Running %s' % (now, command)
+	def rcv(self, message, target):
+		now = str(datetime.datetime.now())
+		print '[%s] RCV: Received "%s" from %s' % (now, message, target)
+	def send(self, message, target):
+		now = str(datetime.datetime.now())
+		print '[%s] SEND: Sending "%s" to %s' % (now, message, target)
+
