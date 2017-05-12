13ebbc0d5503ca19473f0e042de91d1407261e8d
Desobediente Civil <desci@riseup.net>
2017-05-12 05:50:37 -0300

    Bump v0.3.2.b

--- cryptoforexbot/metadata.py
+++ cryptoforexbot/metadata.py
@@ -2,6 +2,6 @@
 handle = '@cryptoforexbot'
 name = 'Crypto Forex Bot'
 agent = 'Telegram Bot - t.me/cryptoforexbot'
-version = '0.3.1.b'
+version = '0.3.2.b'
 source = 'https://github.com/desci/tg-cryptoforexbot'
 bugtracker = 'https://github.com/desci/tg-cryptoforexbot/issues'
