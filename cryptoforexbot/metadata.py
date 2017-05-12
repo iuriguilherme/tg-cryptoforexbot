17254f4d3b9358bf9dc8962aacb0f45bd3d31565
Desobediente Civil <desci@riseup.net>
2017-05-12 08:29:51 -0300

    Bump v0.3.4.b - Added group feedback

--- cryptoforexbot/metadata.py
+++ cryptoforexbot/metadata.py
@@ -2,6 +2,6 @@
 handle = '@cryptoforexbot'
 name = 'Crypto Forex Bot'
 agent = 'Telegram Bot - t.me/cryptoforexbot'
-version = '0.3.3.b'
+version = '0.3.4.b'
 source = 'https://github.com/desci/tg-cryptoforexbot'
 bugtracker = 'https://github.com/desci/tg-cryptoforexbot/issues'
