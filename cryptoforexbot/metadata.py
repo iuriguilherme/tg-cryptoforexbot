6f02482b3403dde3dac524e5f7c84aca9a586162
Desobediente Civil <desci@riseup.net>
2017-05-12 16:20:57 -0300

    Bump v0.4.1.b - Update fiat info

--- cryptoforexbot/metadata.py
+++ cryptoforexbot/metadata.py
@@ -2,6 +2,6 @@
 handle = '@cryptoforexbot'
 name = 'Crypto Forex Bot'
 agent = ''.join(['TelegramBot (', name, ')'])
-version = 'v0.4.0.b'
+version = 'v0.4.1.b'
 source = 'https://github.com/desci/tg-cryptoforexbot'
 bugtracker = 'https://github.com/desci/tg-cryptoforexbot/issues'
