0b761dd695362cd31caa8c62032577e9a2918716
Iuri Guilherme <iuri@fabricadofuturo.com>
2020-01-10 19:43:57 +0000

    Updating coinmarketcap options

--- update.py
+++ update.py
@@ -6,18 +6,19 @@ from plugins.coinmarketcap.api import v1 as api
 
 class update_coinmarketcap():
   def __init__(self):
-    self.api = api()
     self.coinmarketcap()
   def coinmarketcap(self):
     cryptos_file = 'plugins/coinmarketcap/cryptos.json'
-    ticker = self.api.get_ticker()
+    ticker = api.get_ticker(api())
     cryptos = json.load(open(cryptos_file))
 
-    for coin in ticker:
-      if not cryptos.has_key(coin["symbol"]):
-        cryptos.update({coin["symbol"]:{"name":coin["name"],"coinmarketcap_id":coin["id"],"symbols":[coin["id"],coin["symbol"]]}})
-
-    json.dump(cryptos, open(cryptos_file, 'w'), indent=True, sort_keys=True, ensure_ascii=False)
+    if ticker[0]:
+        for coin in ticker[1]:
+          if not cryptos.has_key(coin["symbol"]):
+            cryptos.update({coin["symbol"]:{"name":coin["name"],"coinmarketcap_id":coin["id"],"symbols":[coin["id"],coin["symbol"]]}})
+        json.dump(cryptos, open(cryptos_file, 'w'), indent=True, sort_keys=True, ensure_ascii=False)
+    else:
+        print(u"Didn't worked.")
 
 if __name__ == "__main__":
   update_coinmarketcap = update_coinmarketcap()
